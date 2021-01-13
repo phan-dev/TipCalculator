746a7123d20634f074afa97cd4ff7ba9165b4863
phan-dev <anhnguyenict@gmail.com>
2021-01-13 11:50:00 -0600

    Initial

--- TipCalculator/ContentView.swift
+++ TipCalculator/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var total = "20"
+    @State var tipPercent: Double = 15.0
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Tip Calculator")
+                .font(.title)
+            
+            HStack {
+                Text("$")
+                TextField("Total", text: $total)
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 0.25)
+            }
             .padding()
+            
+            HStack {
+                Slider(value: $tipPercent, in: 1...30, step: 1.0)
+                Text("\(Int(tipPercent))%")
+            }
+            .padding()
+            
+            if let totalNum = Double(total) {
+                Text("Tip Amount: \(totalNum * tipPercent / 100, specifier: "%0.2f")")
+            } else {
+                Text("Please enter a valid numerical value.")
+            }
+            
+        }
     }
 }
 
