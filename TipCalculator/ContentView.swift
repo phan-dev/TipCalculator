6dc7795c7afaba691972691429a97e5c47ed783c
phan-dev <anhnguyenict@gmail.com>
2021-01-13 14:40:52 -0600

    Update UI

--- TipCalculator/ContentView.swift
+++ TipCalculator/ContentView.swift
@@ -8,31 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var total = "20"
+    @State var total = "100"
     @State var tipPercent: Double = 15.0
     
     var body: some View {
         VStack {
             Text("Tip Calculator")
-                .font(.title)
+                .font(.largeTitle)
+                .padding()
             
             HStack {
                 Text("$")
                 TextField("Total", text: $total)
-                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 0.25)
+                    .padding(/*@START_MENU_TOKEN@*/EdgeInsets()/*@END_MENU_TOKEN@*/)
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
             }
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
             .padding()
             
             HStack {
-                Slider(value: $tipPercent, in: 1...30, step: 1.0)
+                Slider(value: $tipPercent, in: 1...100, step: 1.0)
                 Text("\(Int(tipPercent))%")
             }
+            .font(.title)
             .padding()
             
             if let totalNum = Double(total) {
-                Text("Tip Amount: \(totalNum * tipPercent / 100, specifier: "%0.2f")")
+                Text("Tip Amount: $\(totalNum * tipPercent / 100, specifier: "%0.2f")")
+                    .font(.title)
             } else {
-                Text("Please enter a valid numerical value.")
+                Text("Please enter a valid numerical value")
             }
             
         }
